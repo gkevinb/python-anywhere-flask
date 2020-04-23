dd1c9a516c6e469aeeb8e2af826e51aa5e833e28
Gabor Kevin Barta <gkevinba@gmail.com>
2020-04-24 01:05:53 +0200

    Create connection to development server database

--- README.md
+++ README.md
@@ -15,6 +15,8 @@ To shut it down, CTRL + c but also run following to make sure it stopped
 docker-compose down
 ```
 
+https://stackoverflow.com/questions/57642582/mysql-connector-errors-databaseerror-2003-hy000-cant-connect-to-mysql-serve
+
 ## Pythonanywhere
 
 https://www.pythonanywhere.com/batteries_included/
--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,36 @@
-from flask import Flask
 import sys
 import socket
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{username}:{password}@{hostname}/{databasename}".format(
+    username="root",
+    password="admin",
+    hostname="db123",
+    databasename="db",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+
+    __tablename__ = "person"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello world, Flask: {0}! on {1}'.format(sys.version, socket.gethostname())
+    joe = Person.query.first()
+    return 'Hello world, Flask: {0}! on {1} and db: {2}!'.format(sys.version, socket.gethostname(), joe.name)
 
 @app.route('/health')
 def health():
