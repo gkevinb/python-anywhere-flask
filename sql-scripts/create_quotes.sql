797e89188a137eaa72b8dca783b175062cbcbf8d
Gabor Kevin Barta <gkevinba@gmail.com>
2020-05-19 20:34:59 +0200

    Add additional quotes to SQL script

--- sql-scripts/create_quotes.sql
+++ sql-scripts/create_quotes.sql
@@ -12,8 +12,11 @@ create table quote (
     primary key (id)
 );
 
-insert into quote (body, author, category) value ("Use the talents you possess, for the woods would be very silent if no birds sang except the best.", "Henry Van Dyke", "Historical");
+insert into quote (body, author, category, subcategory, numeral) value ("Use the talents you possess, for the woods would be very silent if no birds sang except the best.", "Henry Van Dyke", "Historical", null, null);
 insert into quote (body, author, category, subcategory, numeral) value ("The fault, dear Brutus, is not in our stars, But in ourselves, that we are underlings.", "Cassius", "Shakespeare", "Julius Caesar", "Act I, Scene III, 140-141");
-
+insert into quote (body, author, category, subcategory, numeral) value ("People do not decide their futures, they decide their habits and their habits decide their future.", "F. Matthias Alexander", "Historical", null, null);
+insert into quote (body, author, category, subcategory, numeral) value ("Pleasure in the job puts perfection in the work.", "Aristotle", "Historical", "Ancient Greece", null);
+insert into quote (body, author, category, subcategory, numeral) value ("The meaning of life is to find your gift. The purpose of life is to give it away.", "David Viscott", "Historical", null, null);
+insert into quote (body, author, category, subcategory, numeral) value ("Besides the noble art of getting things done, there is the noble art of leaving things undone. The wisdom of life consists in the elimination of non-essentials", "Lin Yutang", "Historical", null, null);
 
 select * from quote;
